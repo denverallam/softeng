b686afa42e2c0146e0279977135db4b973cb82a5
denverallam <denver.allam.iics@ust.edu.ph>
2021-04-24 23:37:52 +0800

    with search box

--- client/src/components/Admin/Content/ContentList.js
+++ client/src/components/Admin/Content/ContentList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Content from './Content';
-import {  Input, ListGroup, ListGroupItem } from 'reactstrap';
+import { Input, ListGroup, ListGroupItem } from 'reactstrap';
 import { getAllContent, deleteContent } from '../../../actions/contentActions';
 import Load from '../../Content/Load';
 import Dashboard from '../Dashboard';
@@ -16,11 +16,31 @@ const ContentList = () => {
     const loading = useSelector(state => state.content.loading)
     const [order, setOrder] = useState('')
     const [contentList, setContentList] = useState([])
+    const [isSearching, setIsSearching] = useState(false)
+
+    const [countMessage,setCountMessage] = useState("")
 
     const deleteItem = (id) => {
         setContentList(content.contentList.filter(content => content._id !== id))
         dispatch(deleteContent(id))
     }
+
+    const filterSearch = (input) => {
+        setContentList(content.contentList.filter(content => {
+            return content.title.toLowerCase().includes(input.toLowerCase())
+        }))
+
+        if(!contentList){
+            setCountMessage("No match found")
+        }
+        if(input){
+            setIsSearching(true)
+        }
+        else{
+            setIsSearching(false)
+        }
+    }
+
 
     listSorter(order, content.contentList)
 
@@ -32,32 +52,40 @@ const ContentList = () => {
 
     useEffect(() => {
         setContentList(content.contentList)
-    }, [content])
+    }, [content, listSorter,order])
+
+
+
 
     return (
         <>
             <Dashboard />
+            <div className="container mt-2">
+                <Input type="text" name="title" id="title" onChange={(e) => filterSearch(e.target.value)} />
+            </div>
             <div>
                 {
                     loading ? <Load /> :
-                    content.contentList.length > 0 ?
+                        contentList.length > 0 ?
                             <>
-                                {content.contentList.length > 1 ?
-                                        <Order setValue={setOrder}/>
-                                        :
+                                {contentList.length > 1 ?
+                                    <Order setValue={setOrder} />
+                                    :
                                     <></>
                                 }
-                                <ListGroup>
-                                    {
-                                        contentList.map(content => (
-                                            <ListGroupItem className="border-0" key={content._id}>
-                                                <Content content={content} deleteContent={deleteItem} />
-                                            </ListGroupItem>
-                                        ))
-                                    }
-                                </ListGroup>
+                                <>
+                                    <ListGroup>
+                                        {
+                                            contentList.map(content => (
+                                                <ListGroupItem className="border-0" key={content._id}>
+                                                    <Content content={content} deleteContent={deleteItem} />
+                                                </ListGroupItem>
+                                            ))
+                                        }
+                                    </ListGroup>
+                                </>
                             </> :
-                            <p className="text-center">No articles posted</p>
+                             <p className="text-center">{isSearching ? "No match found" : "No articles posted"}</p>
                 }
             </div >
         </>
