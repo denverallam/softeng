cc8b6668a9dc5c1f963872384b9b392a19d9801c
denverallam <denver.allam.iics@ust.edu.ph>
2021-05-10 10:56:07 +0800

    v4.3

--- client/src/components/About/Escolario/EditorialBoard.js
+++ client/src/components/About/Escolario/EditorialBoard.js
@@ -6,15 +6,14 @@ const EditorialBoard = ({ officer }) => {
     const toggle = () => setModal(!modal);
 
     return (
-            <div className="row col-sm-6 mb-4 mx-auto text-center">
-                <div className="col-sm-4">
-                    <CardImg src={officer.image} className="img-fluid rounded" />
-                </div>
-                <Card className="my-2 col-sm-8 border-0" onClick={toggle}>
-                    <CardTitle className="board-name">{officer.name}</CardTitle>
-                    <CardSubtitle className="board-position">{officer.position}</CardSubtitle>
-                </Card>
+        <div className="row col-sm-6 mb-4 mx-auto text-center">
+            <div className="col-sm-4 mx-auto" style={{ backgroundColor: '#002e5d', backgroundImage: `url(${officer.image})`, backgroundRepeat: 'no-repeat', backgroundSize: '100%', backgroundPosition: 'center center', minHeight: '100px', width: '100px', maxWidth:'100px' }}>
             </div>
+            <Card className="my-2 col-sm-8 border-0" onClick={toggle}>
+                <CardTitle className="board-name">{officer.name}</CardTitle>
+                <CardSubtitle className="board-position">{officer.position}</CardSubtitle>
+            </Card>
+        </div>
     )
 }
 
